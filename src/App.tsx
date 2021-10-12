04c4a90fc4065de952262b4549098274a8235447
lucas_mattos <lucasmattos251012@gmail.com>
2021-10-12 00:02:03 -0300

    Sistema de finanças

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import  * as C from "./App.styles";
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText> Sistema Financeiro </C.HeaderText>
+      </C.Header>
+
+      <C.Body>
+        
+        {/** area de informações */}
+
+        {/** area de inserção */}
+
+        {/** tabela de itens */}
+
+      </C.Body>
+
+    </C.Container>
+  );
+}
+
+export default App;
